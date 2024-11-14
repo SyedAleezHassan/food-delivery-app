04b02dabfde084e0012acfc782023096dfcb4337
SyedAleezHassan <syedaleezhassan@gmail.com>
2024-11-14 15:00:26 +0500

    welcome page done

--- lib/screens/get Started/welcomeScreen.dart
+++ lib/screens/get Started/welcomeScreen.dart
@@ -13,6 +13,42 @@ class getStarted extends StatelessWidget {
               top: 140,
               left: 30,
               child: Image.asset('assets/images/Food for Everyone.png')),
+          Positioned(
+              top: 140,
+              left: 30,
+              child: Image.asset('assets/images/Food for Everyone.png')),
+          Positioned(
+              top: 350,
+              right: -19,
+              child: Image.asset('assets/images/boy.png')),
+          Positioned(top: 280, child: Image.asset('assets/images/girl.png')),
+          Positioned(
+              top: 520, child: Image.asset('assets/images/girl shade.png')),
+          Positioned(
+              top: 520,
+              right: 0,
+              child: Image.asset('assets/images/bot shade.png')),
+          Positioned(
+            bottom: 35,
+            right: 70,
+            child: Container(
+              height: 50, // increased height for better appearance
+              width: 240, // increased width for better appearance
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(19),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                "Get Started",
+                style: TextStyle(
+                  color: Color(0xffbFF460A),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16, // adjust font size as needed
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
